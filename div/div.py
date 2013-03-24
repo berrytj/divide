e0e40a202c5dace8e8831415b70edb64844733cf
Tom Berry <tberry860@gmail.com>
2013-03-23 18:06:36 -0700

    added comments

--- div/div.py
+++ div/div.py
@@ -1,6 +1,9 @@
 
 def divide(num, den):
-	''' '''
+	'''Accept a numerator and a denominator.  Return
+	quotient and remainder in special cases.  In all
+	other cases, dictate a range of possible solutions
+	and initiate binary search.'''
 
 	if den == 0:
 		return 'Error', 'Error'
@@ -9,48 +12,45 @@ def divide(num, den):
 	elif den == 1:
 		return num, 0
 
-	sign = cmp(num * den, 0)
-	inner = 0
-	outer = sign * abs(num)
+	sign = cmp(num * den, 0)  # The sign that the correct quotient must have.
+	outer = sign * abs(num)   # The outside of the range to search within.
+	inner = 0                 # The inside of the range to search within.
 
-	return find(num, den, inner, outer, sign)
+	return find_quotient(num, den, inner, outer, sign)
 
 
-def find(num, den, inner, outer, sign):
-	''' '''
-	#print 'inner = ' + str(inner) + ', outer = ' + str(outer)
-	#mid = divide(inner + outer, 2)
-	mid = (inner + outer) // 2
-	print 'mid = ' + str(mid)
-	estimate = mid * den
-	print 'estimate: ' + str(estimate)
-	remainder = num - estimate
+def find_quotient(num, den, inner, outer, sign):
+	'''Bisect the range of possible solutions, returning
+	the bisector if the corresponding remainder is between
+	zero and the denominator.  In all other cases, repeat
+	binary search on the appropriate half of the range.'''
+
+	mid = (inner + outer) // 2  # Bisect the range being searched.
+	remainder = num - mid * den
 
 	if remainder == 0:
-		return mid, remainder
+		return mid, remainder  # Return solution.
+
 	elif cmp(remainder, 0) == cmp(den, 0):
+	# If remainder and denominator have the same sign:
+
 		if 0 < abs(remainder) < abs(den):
-			return mid, remainder
-		else:
+			return mid, remainder  # Return solution.
+
+		else:  # If remainder is `absolutely` larger than the denominator:
 			if sign == 1:
-				inner = mid + 1  #outer range if quotient should be positive
+				inner = mid + 1  # Search outer range.
 			else:
-				outer = mid + 1  #narrow range if quotient should be negative
-	else:
+				outer = mid + 1  # Search inner range.
+
+	else:  # If remainder has a different sign than the denominator:
+
 		if sign == 1:
-			outer = mid - 1  #narrow range if quotient should be positive
+			outer = mid - 1  # Search outer range.
 		else:
-			inner = mid - 1  #outer range if quotient should be negative
-		
-
-	return find(num, den, inner, outer, sign)
-
-
-
-
-
-
-
+			inner = mid - 1  # Search inner range.
+	
+	return find_quotient(num, den, inner, outer, sign)
 
 
 
--- tests/div_tests.py
+++ tests/div_tests.py
@@ -3,9 +3,15 @@ from nose.tools import *
 from div.div import divide
 
 def test_divide():
+	'''Test the `divide` function by inputting a wide
+	range of integers and comparing the outcomes with
+	the results of performing native calculations.'''
 
-	for num in range(-10, 10):
-		for den in range(-10, 10):
+	# Test a wide range of both positive and negative integers.
+	ints = range(-1050, -950) + range(-100, 100) + range(950, 1050)
+
+	for num in ints:
+		for den in ints:
 
 			if den == 0:
 				exp_quotient = 'Error'
@@ -14,8 +20,9 @@ def test_divide():
 				exp_quotient = num // den
 				exp_remainder = num % den
 
-			print 'num: ' + str(num) + ', den: ' + str(den)
 			quotient, remainder = divide(num, den)
 
 			assert_equal(exp_quotient, quotient)
 			assert_equal(exp_remainder, remainder)
+			
+
